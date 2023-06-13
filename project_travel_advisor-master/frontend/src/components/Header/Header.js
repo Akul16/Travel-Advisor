7e46723c5c5ec21ba1493f6b23a2459b64211a67
Akul16 <akuldpatel16@gmail.com>
2023-06-12 21:13:33 -0400

    commit

---
+++ project_travel_advisor-master/frontend/src/components/Header/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Autocomplete } from '@react-google-maps/api';
+import { AppBar, Toolbar, Typography, InputBase, Box } from '@material-ui/core';
+import SearchIcon from '@material-ui/icons/Search';
+import "./header.css"
+// import axios from 'axios';
+
+import useStyles from './styles.js';
+
+const Header = ({ onPlaceChanged, onLoad }) => {
+  const classes = useStyles();
+
+  return (
+    <AppBar position="static">
+      <Toolbar className={classes.toolbar}>
+        <Typography variant="h5" className={classes.title}>
+          Travel Advisor
+        </Typography>
+        <Box display="flex">
+
+          <Typography variant="h6" className={classes.title}>
+            Explore new places
+          </Typography>
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+            </div>
+          </Autocomplete>
+        </Box>
+        <form action="https://travel-blog-40.netlify.app/">
+                <button>Travel Stories</button>
+            </form>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
